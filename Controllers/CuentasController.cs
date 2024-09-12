28d93c135c3a3982760887a4a46150b75c433484
Alan250812GPR <alan.2500gpr@gmail.com>
2024-09-12 00:28:34 -0600

    Base de controladores

---
+++ Controllers/CuentasController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Farmacias.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CuentasController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CuentasController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        //[ServiceFilter(typeof(FiltersMid))]
+        //[Authorize(Roles = "Super Admin")]
+        [HttpGet]
+        public string Get()
+        {
+            return "Running";
+        }
+    }
+}
---
+++ Controllers/EmpresasController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Farmacias.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpresasController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public EmpresasController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        //[ServiceFilter(typeof(FiltersMid))]
+        //[Authorize(Roles = "Super Admin")]
+        [HttpGet]
+        public string Get()
+        {
+            return "Running";
+        }
+    }
+}
